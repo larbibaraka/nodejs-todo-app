82858909b86b182d0aebaff409647aa73909636c
larbibaraka <laridev@gmail.com>
2018-12-22 11:43:24 +0100

    adding some stuff

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+//require the controller
+var todoController = require('./controllers/todoController');
+
+
+var app = express();
+//set the template engine
+app.set('view engine' , 'ejs');
+//set the assets folder
+app.use(express.static('./public/assets/'));
+//the welecome page
+app.get('/',function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+//fire the controller
+todoController(app);
+
+app.listen('3000');
